e2abeb949cdd473ec88eea64323d72c4e8bfbef5
Tim Schartman <tschartman@yahoo.com>
2020-01-26 13:10:09 -0600

    initialize spring repository

---
+++ src/test/java/com/example/mediacenter/MediaCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mediacenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediaCenterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
